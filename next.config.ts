d5b8e02aa50d015f78bd5f131f209257964c4d68
nontprawitch <nontprawitch.saetang@gmail.com>
2026-01-31 19:35:54 +0700

    fix path

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactCompiler: true,
   output: 'export',
+  basePath: '/nextjs-portfolio',
+  assetPrefix: '/nextjs-portfolio',
   images: {
     unoptimized: true
-  }
+  },
 };
 
 export default nextConfig;
